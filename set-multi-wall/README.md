bb4143b04f2d85e1e78507c67ab7026dd3ad6845
Andrew L. Ayers <junkbotix@gmail.com>
2016-04-06 21:36:42 -0700

    Initial commit.

---
+++ set-multi-wall/README.md
@@ -0,0 +1,85 @@
+# set-multi-wall.sh - Multi-monitor background image switcher#
+
+### Features ###
+
+* Uses feh (https://feh.finalrewind.org/) to update backgrounds
+* Uses imagemagick (http://www.imagemagick.org/script/index.php) for transition effect
+* Currently only handles two monitors (could be expanded to handle more)
+* Easy to configure (see below)
+
+### About ###
+
+After configuring my Ubuntu 14.04 LTS box to look and work more like #! (which 
+at the time was still kinda long-in-the-tooth - then it was shut down - then 
+ressurected - but not quite - meh), I wanted to have means to update my 
+backgrounds dynamically. 
+
+Nothing that I could find would do everything I wanted; mainly I wanted random 
+images, on different monitors, fade-in/out, configurable delay between images. 
+Most tools only did a couple of those features, or only changed the background
+on startup. Maybe I needed to look around some more, but I figured the time might
+be better spent coming up with my own solution.
+
+For some reason (because I am a masochist?) I wanted to do this using a Bash
+script. I contemplated Perl, I thought about PHP, I considered Python, C/C++ 
+seemed like overkill. I thought that Bash might be perfect, but I still needed a
+way to get an image to the background. A bit of research on #! and Arch Linux gave
+me the answer. To the rescue came feh.
+
+After a bit of openbox config hacking I got something working, but the screen 
+transitions were jarring (especially when you have transparent windows involved). 
+So I set out to fix that issue. My first try worked well, but the transitions were 
+kinda jerky. They weren't as jarring as before, but not perfect. 
+
+I left things like that for a long while, then I recently got a dual-monitor 
+stand, and decided to turn one of my monitors to a portrait orientation (great
+for code and reading long docs). Unfortunately, my backgrounds no longer worked
+properly, to put it simply. The main issue was centering, which I knew was
+going to be an easy fix to the feh command. So, I decided to fix that issue, and 
+while I was in there, a few others, and what I came up with is what you see here.
+
+### Configuration ###
+
+To set it all up, put the script somewhere and make it executable. Modify the 
+lines in the script to point to where your background images are stored (WALLPAPERS), 
+and to determine how often you want to swap images (WAIT - default is 180 seconds, 
+or 3 minutes).
+
+Create your wallpapers.cfg file, and put it in the path set by WALLPAPERS. For my
+system, I then modified my openbox autostart config file ($HOME/.config/openbox/autostart.sh) to 
+kick off the program when I logged in:
+
+($HOME/set-multi-wall.sh) &
+
+You may need to do something different, but it shouldn't be anything too difficult 
+to set up. If you need some help, give me a shout (see below), I'll see what I 
+can do.
+
+### TODO ###
+
+* Configurable number of monitors?
+* Different image transition effects?
+* Window border colors to match background?
+* Method to remotely shut down ricers with fart-can exhausts?
+
+### License ###
+
+Copyright (C) 2016 by Andrew L. Ayers
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later 
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT 
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+this program; if not, write to the Free Software Foundation, Inc., 51 Franklin 
+Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+### Who do I cuss out? ###
+
+* Andrew L. Ayers - junkbotix@gmail.com
+* http://www.phoenixgarage.org/
---
+++ set-multi-wall/set-multi-wall.sh
@@ -0,0 +1,221 @@
+#!/bin/bash
+#
+# set-multi-wall.sh - Multi-monitor background image switcher
+#
+# Copyright (C) 2016 by Andrew L. Ayers
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later 
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT 
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with 
+# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin 
+# Street, Fifth Floor, Boston, MA 02110-1301  USA
+#
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Change WALLPAPERS and WAIT as needed
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# Path to wallpapers
+WALLPAPERS="/path/to/your/wallpapers"
+
+# Transition time (in seconds)
+WAIT=180
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# Last paths selected (used to avoid fade in/out of a current displayed image)
+LAST_PATH0=""
+LAST_PATH1=""
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+show_old_background() {
+    # First run, so if a prior last (old) background exists, show it
+    if [ -s "/tmp/last0.bg" ]; then
+        feh --image-bg black --bg-max /tmp/last0.bg /tmp/last1.bg
+    else
+        # Otherwise, grab a new set of background(s)    
+        show_new_background
+    fi
+}
+
+show_new_background() {
+    # randomly get a section
+    get_random_section
+    
+    local SECTION=$RVAL
+    
+    # randomly select a line
+    get_random_line $SECTION
+    
+    local WPATH=$WALLPAPERS/$SECTION
+    local WORDS=( ${RVAL[@]} )
+    local WORD0=${WORDS[0]} 
+    local WORD1=${WORDS[1]}
+
+    # randomly get files
+    get_random_files $WPATH $WORD0 $WORD1
+
+    local FILES=( ${RVAL[@]} )
+    local PATH0=$WPATH/"${WORD0/\*/${FILES[0]}}" 
+    local PATH1=$WPATH/"${WORD1/\*/${FILES[1]}}"
+
+    fade_new_background $PATH0 $PATH1
+}
+
+get_random_section () {
+    # Extract all sections
+    local SECTIONS=( `egrep "\\[.*\\]" $WALLPAPERS/wallpapers.cfg` )
+
+    # Pick a random section
+    local RANGE=${#SECTIONS[*]}
+    local SHOW=$(( $RANDOM % $RANGE ))
+    local SECTION=${SECTIONS[$SHOW]}
+
+    RVAL=(`echo $SECTION | tr -d '[]'`)
+}
+
+get_random_line () {
+    local SECTION=$1
+    
+    # Set IFS to break on linefeeds, then put in array:
+    #
+    #   1. get control file
+    #   2. Use sed to find section
+    #   3. Use sed to remove 1st and last lines
+    #   4. Use sed to skip blank lines
+    #
+    IFS=$'\n'
+    local LINES=( `cat $WALLPAPERS/wallpapers.cfg | sed -n "/\[$SECTION\]/,/\[/p" | sed '1d;$d' | sed '/^$/d'` )
+
+    local RANGE=${#LINES[*]}
+    local SHOW=$(( $RANDOM % $RANGE ))
+    local LINE=${LINES[$SHOW]}
+
+    # Reset IFS to default (space, tab, newline)
+    # and extract WORDS from LINE to an array
+    IFS=$' ^I$'
+    RVAL=( `echo $LINE` )
+}
+
+get_random_files() {
+    local WPATH=$1
+    
+    local WORD0=$2
+    local WORD1=$3
+
+    # Extract all horizontal files
+    local FILES0=($WPATH/$WORD0)
+    local FILES0=("${FILES0[@]##*/}") 
+
+    # Extract all vertical files
+    local FILES1=($WPATH/$WORD1)
+    local FILES1=("${FILES1[@]##*/}") 
+
+    # Pick a random horizontal file
+    local RANGE=${#FILES0[@]}
+    local SHOW=$(( $RANDOM % $RANGE ))
+    local FILE0=${FILES0[$SHOW]}
+
+    # Pick a random vertical file
+    local RANGE=${#FILES1[@]}
+    local SHOW=$(( $RANDOM % $RANGE ))
+    local FILE1=${FILES1[$SHOW]}
+    
+    # Set IFS to break on linefeeds, then put in array
+    IFS=$'\n'
+    RVAL=( `echo $FILE0; echo $FILE1` )
+}
+
+fade_new_background() {
+    local PATH0=$1
+    local PATH1=$2
+
+    fade_new_backgrounds $PATH0 $PATH1
+    
+    show_new_backgrounds $PATH0 $PATH1
+}
+
+fade_new_backgrounds() {
+    # Copy the selected backgrounds to the new backgrounds
+    cp $1 /tmp/new0.bg
+    cp $2 /tmp/new1.bg
+    
+    # Fade transitions for last to new backgrounds
+    convert /tmp/last0.bg -fill black -colorize 25% /tmp/tran01.bg
+    convert /tmp/last1.bg -fill black -colorize 25% /tmp/tran11.bg
+
+    convert /tmp/last0.bg -fill black -colorize 50% /tmp/tran02.bg
+    convert /tmp/last1.bg -fill black -colorize 50% /tmp/tran12.bg
+
+    convert /tmp/last0.bg -fill black -colorize 75% /tmp/tran03.bg
+    convert /tmp/last1.bg -fill black -colorize 75% /tmp/tran13.bg    
+
+    convert /tmp/new0.bg -fill black -colorize 50% /tmp/tran04.bg
+    convert /tmp/new1.bg -fill black -colorize 50% /tmp/tran14.bg    
+
+    convert /tmp/new0.bg -fill black -colorize 25% /tmp/tran05.bg
+    convert /tmp/new1.bg -fill black -colorize 25% /tmp/tran15.bg    
+}
+
+show_new_backgrounds() {
+    local PATH0=$1
+    local PATH1=$2
+    local DISPLAY0="/tmp/new0.bg";
+    local DISPLAY1="/tmp/new1.bg";
+
+    for i in `seq 1 5`; do
+        # Transition both displays (most common)
+        if [ "$PATH0" != "$LAST_PATH0" ] && [ "$PATH1" != "$LAST_PATH1" ]; then
+            feh --image-bg black --bg-max /tmp/tran0$i.bg /tmp/tran1$i.bg
+            continue
+        fi
+    
+        # Transition display 0 only
+        if [ "$PATH0" != "$LAST_PATH0" ] && [ "$PATH1" == "$LAST_PATH1" ]; then
+            feh --image-bg black --bg-max /tmp/tran0$i.bg $DISPLAY1
+            continue
+        fi
+
+        # Transition display 1 only
+        if [ "$PATH0" == "$LAST_PATH0" ] && [ "$PATH1" != "$LAST_PATH1" ]; then
+            feh --image-bg black --bg-max $DISPLAY0 /tmp/tran1$i.bg
+        fi        
+    done
+
+    # Set new background (full brightness)
+    feh --image-bg black --bg-max /tmp/new0.bg /tmp/new1.bg
+    
+    # Set last backgrounds to current backgrounds
+    mv /tmp/new0.bg /tmp/last0.bg; mv /tmp/new1.bg /tmp/last1.bg
+    
+    # Remove transition backgrounds
+    rm /tmp/tran*.bg
+    
+    LAST_PATH0=$PATH0
+    LAST_PATH1=$PATH1
+}
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# Set the current display (necessary when kicked off at login)
+DISPLAY=:0.0
+
+while true; do
+    if [ -z "$RUNNING" ]; then
+        show_old_background        
+    else
+        show_new_background
+    fi
+    
+    RUNNING="yes"
+
+    sleep $WAIT
+done
